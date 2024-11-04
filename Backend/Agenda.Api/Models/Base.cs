dc3d4014bcf4a8f4676924c825723df19178debf
monica88lima <flima.monica@gmail.com>
2024-11-04 10:25:22 -0300

    Estrutura Inicial

---
+++ Backend/Agenda.Api/Models/Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public abstract class Base
+    {
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+    }
+}
